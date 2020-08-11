1f2934400fd6dddd35f1746fb254d1fc1e595262
Rod Johnson <rodmjay@ideafortune.com>
2020-08-11 13:16:49 -0600

    cleanup

--- src/AgencyPro/src/AgencyPro.Data/Entities/Identity/UserAccountMap.cs
+++ src/AgencyPro/src/AgencyPro.Data/Entities/Identity/UserAccountMap.cs
@@ -1,7 +1,6 @@
 ﻿// // Copyright (c) Rod Johnson & IdeaFortune. All rights reserved.
 // // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using System;
-using System.Collections.Generic;
 using AgencyPro.Core.UserAccount.Models;
 using AgencyPro.Data.EFCore;
 using Microsoft.AspNetCore.Identity;
